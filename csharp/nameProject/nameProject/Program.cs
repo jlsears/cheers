a5a7f012853d8299e2a7f119a42c1c86bff7e900
jlsears <jen.lauren.sears@gmail.com>
2015-07-14 22:58:26 -0500

    beginnings

---
+++ csharp/nameProject/nameProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace nameProject
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.Write ("What's your name?");
+
+			string theAge = string.somethinghere (Console.ReadLine());
+
+			for(string i = 0; i < somethinghere.length; ++i)
+			{
+				Console.Write();
+			}
+
+			Console.WriteLine();
+		}
+	}
+}
