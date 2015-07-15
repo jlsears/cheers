f7888f245518f709a4fc8e43190333eee669562d
jlsears <jen.lauren.sears@gmail.com>
2015-07-14 19:25:18 -0500

    Cheering folks on

---
+++ csharp/namingcheer.js
@@ -0,0 +1,15 @@
+
+var getName = function() {
+  
+var yourName = prompt("What is your name?");
+
+  for (var i = 0; i < yourName.length; i++) {
+    console.log("Can I get a " + yourName[i] + "?");
+  };
+  
+  console.log(yourName + " is such hot stuff!!!");
+
+};
+
+
+
